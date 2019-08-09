b488270383f242d7219b814de7223a167d1c6d4c
Andrew <andydrew313@gmail.com>
2019-08-09 13:29:44 -0700

    change images to required statics

--- VaroDriveIOS/config/images/images.js
+++
@@ -1,9 +0,0 @@
-export default {
-  background: 'https://i.imgur.com/eS4IxK3.png',
-  usernameIcon: 'http://cdn.onlinewebfonts.com/svg/img_189000.png',
-  passwordIcon: 'https://mbtskoudsalg.com/images/password-icon-png-5.png',
-  emailIcon: 'https://png.icons8.com/metro/1600/email.png',
-  logo: 'http://cdn.onlinewebfonts.com/svg/img_79140.png',
-  varologo: 'https://www.varoinc.com/wp-content/uploads/2017/12/cropped-Varo-Logo-ind.png',
-  sideMenuIcon: 'https://cdn3.iconfinder.com/data/icons/mobile-banking-ver-4a/100/1-48-512.png',
-}
